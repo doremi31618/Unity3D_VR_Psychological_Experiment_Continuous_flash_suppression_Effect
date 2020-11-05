1e02e04706bc51f4a01d4ee02518596291d6ddde
Eric Zhan <doremi31618@gmail.com>
2020-11-05 16:08:22 +0800

    add scriptable Object / player data / streaming asstes folder

---
+++ Assets/Scripts/PlayerData/PlayerData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public GameObject prefab;
+    private GameObject clone;
+    public static bool isClone = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 
+        if (!isClone){
+
+            isClone=true;
+            clone = (GameObject)Instantiate(prefab);
+            DontDestroyOnLoad(clone);
+
+        }
+    }
+}
+
---
+++ Assets/Scripts/PlayerData/PlayerDataStruct.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDataStruct : ScriptableObject
+{
+    public enum WeakEye{Right, Left};
+    public WeakEye WeakEyeType = WeakEye.Left;
+    public AnimationCurve FadingCurve;
+    public float fadingTime;
+}
