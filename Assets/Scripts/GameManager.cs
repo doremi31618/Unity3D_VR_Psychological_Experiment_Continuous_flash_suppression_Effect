98da39d8b6d8b382b7cdcdac113f4acc38a77556
Eric Zhan <doremi31618@gmail.com>
2020-11-05 11:59:56 +0800

    test cfs effect

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour
+{
+    public MeshRenderer rightEyeImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z)){
+            rightEyeImage.material.DOFade(0.2f, 50f);
+        }else if(Input.GetKeyDown(KeyCode.X)){
+            rightEyeImage.material.DOFade(0, 25);
+        }
+    }
+}
---
+++ Assets/Scripts/VideoController/ImageEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ImageEffect : MonoBehaviour
+{
+    public void FadeIn(UnityEngine.UI.Image img, float time){
+        img.material.DOFade(1, time);
+    }
+    public void FadeOut(Image img, float time){
+        img.material.DOFade(0, time);
+    }
+
+}
